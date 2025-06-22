5bacb9fb4a8f4a2404e719c103abfc29a3047612
Li Yiu Yeung <rayli26go@gmail.com>
2025-06-22 16:11:39 +0800

    upadte config

--- out/config.js
+++ out/config.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-// you have to update this config file to your own Postgres Database
 exports.config = {
     host: "localhost",
     port: 5432,
@@ -10,4 +9,5 @@ exports.config = {
     database: "postgres",
     connection_limit: 100,
     rapidapi: "367c6f172emsha1d61f8c6dd354cp13041fjsn3abc4f7852ef",
+    rapidapiHost: "google-flights2.p.rapidapi.com",
 };
