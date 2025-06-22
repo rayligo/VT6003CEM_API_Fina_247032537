5f29d51ac1c1eed7bd658b0730fe7b9e747e1bb5
Li Yiu Yeung <rayli26go@gmail.com>
2025-06-22 16:09:06 +0800

    upadte config

--- config.ts
+++ config.ts
@@ -1,4 +1,3 @@
-// you have to update this config file to your own Postgres Database
 export const config = {
   host: "localhost",
   port: 5432,
@@ -7,4 +6,5 @@ export const config = {
   database: "postgres",
   connection_limit: 100,
   rapidapi: "367c6f172emsha1d61f8c6dd354cp13041fjsn3abc4f7852ef",
+  rapidapiHost: "google-flights2.p.rapidapi.com",
 };
